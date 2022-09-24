55af75515174d6d338ee15e7ffcf4ac92f4aa952
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-09-25 04:24:38 +0530

    The Python Type Hierarchy
    Theory only

---
+++ Pyhon 3 - Deep Dive - Part 1 - Functional/5. The Python Type Hierarchy.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 04:13:20 2022
+
+@author: satvik
+"""
+
+#Numbers -- Integral and Non-Integral
+
+#Integral -- Integers, Booleans
+#Non-Integral -- Floats, Complex, Decimals, Fractions
+
+
+#Collections -- Sequences, Sets, and Mappings
+
+#Sequences -- Mutable (Lists) and Immutable (Tuples and Strings)
+#Sets -- Mutable (Sets) and Immutable (Frozen Sets)
+#Mappings -- Dictionaries
+
+
+#Callables
+'''User-Defined Functions
+    Generators
+    Classes
+    Instance Methods
+    Class Instances (__call__())
+    Built-in Functions (e.g. len(), open())
+    Built-in Methods (e.g. my_list.append(x))'''
+#Singletons
+'''None
+    NotImplemented
+    Ellipsis (...)
+    '''
