320bfced74049c149d9f2ab73d6fbbfa88bc8604
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-09-27 03:59:26 +0530

    try-except
    try....except
    continue
    finally

--- Pyhon 3 - Deep Dive - Part 1 - Functional/10. The While Loop.py
+++ Pyhon 3 - Deep Dive - Part 1 - Functional/10. The While Loop.py
@@ -5,7 +5,7 @@ Created on Mon Sep 26 19:52:45 2022
 @author: satvik
 """
 
-#while loop
+# while loop
 i = 0
 while i < 5:
     print(i)
@@ -19,7 +19,7 @@ while True:
         break
         print('something after break')
 
-#------------------------------------------
+# ------------------------------------------
 
 '''
 min_length = 2
@@ -39,12 +39,12 @@ while True:
 
 print("Hello, {0}".format(name))
 
-#------------------------------------------
+# ------------------------------------------
 
 a = 0
 while a < 10:
     a += 1
-    if a%2 == 0:
+    if a % 2 == 0:
         continue
     print(a)
 
@@ -63,14 +63,45 @@ if not found:
     l.append(val)
 print(l)
 
-#------------------------------------------
-l= [1, 2, 3]
+# ------------------------------------------
+l = [1, 2, 3]
 val = 10
 idx = 0
 while idx < len(l):
     if l[idx] == val:
         break
     idx += 1
-else:   #this will only run if the while loop ran without encountering the break statement
+else:  # this will only run if the while loop ran without encountering the break statement
     l.append(val)
 print(l)
+
+# ------------------------------------------------------------------------------
+
+# try...except...finally
+a = 10
+b = 0
+try:
+    a/b
+except ZeroDivisionError:
+    print('division by 0')
+finally:
+    print('this always executes')
+
+a = 0
+b = 2
+
+while a < 4:
+    print('-----------')
+    a += 1
+    b -= 1
+
+    try:
+        a / b
+    except ZeroDivisionError:
+        print("{0}, {1} - division by 0".format(a, b))
+        continue
+    finally:
+        print("{0}, {1} - always executes".format(a, b))
+
+    print("{0}, {1} - main loop".format(a, b))
+# --------------------------
